77e9a3002d23a4a4f2dc74c3905f6519a762921b
chrisfran03 <270155795@yoobeestudent.ac.nz>
2022-07-27 14:54:47 +1200

    implemented API call to Dictionary api

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,40 @@
+import axios from "axios";
 import { useState } from 'react';
 import './App.css';
 
 function App() {
   // Declare a new state variable, which we'll call ""
-  const [word, setWord] = useState("");
+  const [word1, setWord] = useState("");
+
+  const DICTIONARY_BASE_URL = "https://api.dictionaryapi.dev/api/v2";
 
   return (
     <div>
-      <h1>
-        Free Dictionary
-      </h1>
-      
+      <h1>Free Dictionary App</h1>
+
       <div>
-        <label>Search for a word</label><br/>
-        <input type="text" id="word" name="word" onChange={e => setWord(e.target.value)}/><br/>
-        <button onClick={search}>
-        Search
-        </button>
+        <label>Search your word</label>
+        <br />
+        <input
+          type="text"
+          id="word"
+          name="word"
+          onChange={(e) => setWord(e.target.value)}
+        />
+        <br />
+        <button onClick={search}>Search</button>
       </div>
 
-      <p>
-        You have entered {word}
-      </p>
+      <p>You have entered {word1}</p>
+
+      <div id="word-meaning">This will show the result</div>
     </div>
   );
 
-  function search(){
-      alert("Search button has been clicked!");
+  function search() {
+    axios.get(DICTIONARY_BASE_URL + "/entries/en/" + word1).then((res) => {
+      console.log(res.data);
+    });
   }
 }
 

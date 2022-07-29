4cfc7d6bb98c4f302a7611cc2c161fef180cdc22
chrisfran03 <270155795@yoobeestudent.ac.nz>
2022-07-29 12:19:00 +1200

    displaying word meaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { useState } from 'react';
+import { useState } from "react";
 import './App.css';
 
 function App() {
   // Declare a new state variable, which we'll call ""
-  const [word1, setWord] = useState("");
+  const [word, setWord] = useState("");
+  const [wordMeaning, setWordMeaning] = useState<undefined | any>(undefined);
 
   const DICTIONARY_BASE_URL = "https://api.dictionaryapi.dev/api/v2";
 
@@ -13,7 +14,7 @@ function App() {
       <h1>Free Dictionary App</h1>
 
       <div>
-        <label>Search your word</label>
+        <label>Search your word here</label>
         <br />
         <input
           type="text"
@@ -25,16 +26,33 @@ function App() {
         <button onClick={search}>Search</button>
       </div>
 
-      <p>You have entered {word1}</p>
+      <p>You have entered {word}</p>
 
-      <div id="word-meaning">This will show the result</div>
+      {wordMeaning === undefined ? (
+        <p>Word not found</p>
+      ) : (
+        <div id="word-meaning">
+          Meaning:{wordMeaning[0].meanings[0].definitions[0].definition}
+          </div>
+      )}
     </div>
   );
 
   function search() {
-    axios.get(DICTIONARY_BASE_URL + "/entries/en/" + word1).then((res) => {
-      console.log(res.data);
+    axios.get(DICTIONARY_BASE_URL + "/entries/en/" + word).then((res) => {
+    setWordMeaning(res.data);
+    })
+    .catch((err) => {
+      console.log("Word meaning not found");
+      setWordMeaning(undefined);
     });
+    axios.get(DICTIONARY_BASE_URL + "/entries/en/" + word).then((res) => {
+      console.log(res.data);
+      })
+      .catch((err) => {
+        console.log("Word meaning not found");
+        setWordMeaning(undefined);
+      });
   }
 }
 

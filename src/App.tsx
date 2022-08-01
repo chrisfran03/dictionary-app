a58dd06a7b51de660db6f9d45b393d6a2402226a
chrisfran03 <270155795@yoobeestudent.ac.nz>
2022-08-02 09:50:05 +1200

    Added UI elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
 import './App.css';
 
 function App() {
-  // Declare a new state variable, which we'll call ""
+  // Declare a new state variable, which we'll call "word"
   const [word, setWord] = useState("");
   const [wordMeaning, setWordMeaning] = useState<undefined | any>(undefined);
 
@@ -11,29 +14,40 @@ function App() {
 
   return (
     <div>
-      <h1>Free Dictionary App</h1>
+      <h1>English Dictionary</h1>
 
       <div>
-        <label>Search your word here</label>
-        <br />
-        <input
-          type="text"
-          id="word"
-          name="word"
-          onChange={(e) => setWord(e.target.value)}
+        <TextField
+          id="search-bar"
+          className="text"
+          value={word}
+          onChange={(prop: any) => {
+            setWord(prop.target.value);
+          }}
+          label="Enter a the word for meaning..."
+          variant="outlined"
+          placeholder="Search..."
+          size="small"
         />
-        <br />
-        <button onClick={search}>Search</button>
+        <IconButton
+          aria-label="search"
+          onClick={() => {
+            search();
+          }}
+        >
+          <SearchIcon style={{ fill: "blue" }} />
+        </IconButton>
       </div>
 
       <p>You have entered {word}</p>
 
-      {wordMeaning === undefined ? (
+      {wordMeaning === undefined || wordMeaning === null ? (
         <p>Word not found</p>
       ) : (
         <div id="word-meaning">
-          Meaning:{wordMeaning[0].meanings[0].definitions[0].definition}
-          </div>
+        Meaning:{wordMeaning[0].meanings[0].definitions[0].definition}  
+         </div>
+         
       )}
     </div>
   );
